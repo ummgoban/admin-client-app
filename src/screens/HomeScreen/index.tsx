49bc62f25d2c03c04288ec7235d8054e2b28f705
99mini <43674669+99mini@users.noreply.github.com>
2024-09-26 01:58:58 +0900

    chore: install deps (#12)
    * chore: install deps
    
    * chore: pod install
    
    ---------
    
    Co-authored-by: mimi99 <0mini9939@gmail.com>

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -21,16 +21,26 @@ const HomeScreen = () => {
 =======
 import React from 'react';
 import {Text, View} from 'react-native';
+<<<<<<< HEAD
 import TagSelector from '@/components/common/TagSelector';
+=======
+
+>>>>>>> b070643 (chore: install deps (#12))
 const HomeScreen = () => {
   return (
     <View>
       <Text>{'HomeScreen'}</Text>
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 2bf2560 (chore: install deps)
 =======
       <TagSelector />
 >>>>>>> ba3ef1b (feat: 태그 드랍다운 컴포넌트 구현)
+=======
+      <TagSelector />
+=======
+>>>>>>> b070643 (chore: install deps (#12))
+>>>>>>> b5e02ba (chore: install deps (#12))
     </View>
   );
 };

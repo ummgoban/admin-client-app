ee1ecb0919ab46f00333dea08ea8fe5188e303fb
99mini <43674669+99mini@users.noreply.github.com>
2024-10-15 15:17:18 +0900

    feat: implement ToggleButtonGroup and ToggleButton Component (#19)
    * feat: toggle button 구현
    
    * fix: change number id to string value
    
    * fix: 토글버튼 height 추가
    
    * fix: resolve merge conflict
    
    * fix: re-order bottom nav
    
    ---------
    
    Co-authored-by: l-lyun <kimdohyun032@gmail.com>

---
+++ src/screens/OrderHistoryScreen/OrderHistoryScreen.style.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/native';
+
+import {ToggleButtonGroup} from '@/components/common';
+
+const NavbarGroup = styled(ToggleButtonGroup)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  margin: 10px;
+`;
+
+const S = {NavbarGroup};
+
+export default S;

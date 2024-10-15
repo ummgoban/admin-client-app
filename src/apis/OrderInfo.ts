1dc5109291a203810532a3a9e5a9f90f10ce136a
l-lyun <142032659+l-lyun@users.noreply.github.com>
2024-10-15 14:33:47 +0900

    feat: 주문 상세 페이지 제작 (#22)
    * feat: 네비게이션 및 타입 작성
    
    * feat: 주문 상세 페이지 구현
    
    * style: 테스트 코드 변경 및 포맷팅
    
    * style: CI error
    
    * style: CI ERROR
    
    * chore: resolve type check github action (#23)
    
    chore: update typecheck
    
    * style: 문자열 리터럴 수정
    
    ---------
    
    Co-authored-by: 99mini <43674669+99mini@users.noreply.github.com>

---
+++ src/apis/OrderInfo.ts
@@ -0,0 +1,78 @@
+import {OrderResponseType} from '@/types/OrderDetailType';
+
+const dummyOrderInfo: OrderResponseType = {
+  code: 200,
+  message: '성공',
+  data: [
+    {
+      id: '1',
+      customerRequset: '일부러 아래로 내렸습니다 디자인 논의 필요 붙일까요',
+      createdAt: '2024-10-14T12:21:31.121Z',
+      pickupReservedAt: '2024-10-14T13:00:00.000Z',
+      ordersPrice: 35000,
+      orderMemberName: '홍길동',
+      orderStatus: '예약완료',
+      products: [
+        {
+          id: 101,
+          image: 'https://legacy.reactjs.org/logo-og.png',
+          name: '갈비찜',
+          originPrice: 40000,
+          discountPrice: 35000,
+          discountRate: 12,
+          count: 1,
+        },
+        {
+          id: 102,
+          image: 'https://legacy.reactjs.org/logo-og.png',
+          name: '떡볶이',
+          originPrice: 5000,
+          discountPrice: 4500,
+          discountRate: 10,
+          count: 2,
+        },
+        {
+          id: 105,
+          image: 'https://legacy.reactjs.org/logo-og.png',
+          name: '떡볶이',
+          originPrice: 5000,
+          discountPrice: 4500,
+          discountRate: 10,
+          count: 2,
+        },
+        {
+          id: 103,
+          image: 'https://legacy.reactjs.org/logo-og.png',
+          name: 'ㅇㅇ',
+          originPrice: 5000,
+          discountPrice: 4500,
+          discountRate: 10,
+          count: 2,
+        },
+        {
+          id: 104,
+          image: 'https://legacy.reactjs.org/logo-og.png',
+          name: '떡볶이',
+          originPrice: 5000,
+          discountPrice: 4500,
+          discountRate: 10,
+          count: 2,
+        },
+      ],
+    },
+  ],
+};
+
+export const getOrderHistory = async (): Promise<OrderResponseType | null> => {
+  try {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(dummyOrderInfo);
+        console.log('fetch tags data');
+      }, 500);
+    });
+  } catch (error) {
+    console.error('Error fetching tag data:', error);
+    return null;
+  }
+};

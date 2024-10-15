6c44ff6137781f5e5403e0dfbf5f340c98020e8b
l-lyun <kimdohyun032@gmail.com>
2024-10-15 16:26:58 +0900

    Revert "chore: resolve type check github action (#23)"
    This reverts commit 40c5910d9896d0e0d7beae35d67ce0041ada396a.

--- src/screens/OrderDetailScreen/index.tsx
+++ src/screens/OrderDetailScreen/index.tsx
@@ -1,11 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import {getOrderHistory} from '@/apis/OrderInfo';
 import S from './OrderDetailScreen.style';
+import OrderCustomerInfo from '../../components/orderDetail/OrderCustomerInfo';
 import {OrderResponseType, OrderInfoType} from '@/types/OrderDetailType';
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+import OrderProductsInfo from '@/components/orderDetail/OrderProductsInfo';
+<<<<<<< HEAD
+>>>>>>> c8880cc (style: 테스트 코드 변경 및 포맷팅)
+import S from './OrderDetailScreen.style';
+import {OrderResponseType, OrderInfoType} from '@/types/OrderDetailType';
+=======
+import OrderProductsInfo from '../../components/orderDetail/OrderProductsInfo';
+>>>>>>> e76a454 (style: CI ERROR)
+>>>>>>> parent of 40c5910 (chore: resolve type check github action (#23))
 import {Text} from 'react-native';
 import OrderCustomerInfo from '@/components/OrderDetail/OrderCustomerInfo';
 import OrderProductsInfo from '@/components/OrderDetail/OrderProductsInfo';
 
+<<<<<<< HEAD
+=======
+=======
+import {Text} from 'react-native';
+>>>>>>> ab51f00 (style: CI error)
+>>>>>>> parent of 40c5910 (chore: resolve type check github action (#23))
 const OrderDetailScreen = () => {
   const [orderData, setOrderData] = useState<OrderInfoType | null>();
 

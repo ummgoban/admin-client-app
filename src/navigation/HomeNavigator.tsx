f6678cdfb50182b6d5c5d85a47bc61e0cdb8e647
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-10-17 00:10:17 +0900

    fix: 탭바 수정 - 메뉴관리 추가

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -4,9 +4,9 @@ import OrderDetailScreen from '@/screens/OrderDetailScreen';
 import {HomeStackParamList} from '@/types/StackNavigationType';
 import {TabBar} from '@components/common';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomeScreen from '@screens/HomeScreen';
 import React from 'react';
 import MyPageScreen from '@/screens/MyPageScreen';
+import MenuManageScreen from '@/screens/MenuManageScreen';
 const Tab = createBottomTabNavigator<HomeStackParamList>();
 
 const HomeNavigator = () => {
@@ -15,7 +15,7 @@ const HomeNavigator = () => {
       <Tab.Screen name="OrderHistory" component={OrderHistoryScreen} />
       <Tab.Screen name="MarketInfo" component={MarketInfoScreen} />
       <Tab.Screen name="OrderDetail" component={OrderDetailScreen} />
-      <Tab.Screen name="Feed" component={HomeScreen} />
+      <Tab.Screen name="MenuManage" component={MenuManageScreen} />
       <Tab.Screen name="MyPage" component={MyPageScreen} />
       {/* <Tab.Screen name="Favorite" component={FavoriteScreen} /> */}
       {/* Add more screens here */}
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,20 +1,18 @@
-// import {RootStackParamList} from '@/types/StackNavigationType';
-// import {useNavigation} from '@react-navigation/native';
-// import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '@/types/StackNavigationType';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {Button} from 'react-native-paper';
 import React from 'react';
-// import {Button} from 'react-native-paper';
-import MenuManageScreen from '../MenuManageScreen';
 import {View} from 'react-native';
 
 const HomeScreen = () => {
-  // const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   return (
     <View>
-      <MenuManageScreen />
-      {/* <Button
+      <Button
         onPress={() => navigation.navigate('Home', {screen: 'MarketInfo'})}>
         가게 관리
-      </Button> */}
+      </Button>
     </View>
   );
 };

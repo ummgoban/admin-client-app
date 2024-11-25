e9efdb345115273ba0cf1e38e32ff2918d4859f2
99mini <itman390@gmail.com>
2024-11-25 14:39:40 +0900

    feat: simplify initial route handling in HomeNavigator by removing unnecessary market fetch

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -3,7 +3,6 @@ import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useEffect} from 'react';
 
-import useMarket from '@/hooks/useMarket';
 import useProfile from '@/hooks/useProfile';
 import MarketInfoScreen from '@/screens/MarketInfoScreen';
 import MenuManageScreen from '@/screens/MenuManageScreen';
@@ -15,47 +14,22 @@ import {
 import {TabBar} from '@components/common';
 import OrderNavigator from './OrderNavigator';
 
-function getInitialRouteName(
-  profile: boolean,
-  market: boolean,
-): keyof RootStackParamList {
-  if (!profile) {
-    return 'Register';
-  }
-  if (!market) {
-    return 'RegisterMarketRoot';
-  }
-  return 'Home';
-}
-
 const Tab = createBottomTabNavigator<HomeStackParamList>();
 
 const HomeNavigator = () => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
   const {profile, fetch: fetchProfile} = useProfile();
-  const {market, fetch: fetchMarketList} = useMarket();
 
   useEffect(() => {
     fetchProfile();
   }, [fetchProfile]);
 
   useEffect(() => {
-    fetchMarketList();
-  }, [fetchMarketList]);
-
-  useEffect(() => {
-    const initialRouteName = getInitialRouteName(
-      Boolean(profile),
-      Boolean(market.length),
-    );
-
-    if (initialRouteName === 'Register') {
-      navigation.navigate(initialRouteName, {screen: 'Login'});
-    } else if (initialRouteName === 'RegisterMarketRoot') {
-      navigation.navigate(initialRouteName, {screen: 'RegisterMarket'});
+    if (!profile) {
+      navigation.replace('Register', {screen: 'Login'});
     }
-  }, [market.length, navigation, profile]);
+  }, [navigation, profile]);
 
   return (
     <Tab.Navigator tabBar={TabBar}>

8f72e7eeed0ee0963bd93fc485ba2694084cf682
l-lyun <142032659+l-lyun@users.noreply.github.com>
2024-12-09 15:44:57 +0900

    fix: admin fcm 기능 보완 및 마이페이지로 이동 (#68)
    * fix: fcm 세팅 변경
    
    * feat: admin fcm 추가 완료
    
    * fix: tsc error
    
    * fix: yarn tsc error
    
    * fix: fcm.ts로 이름 복구

--- App.tsx
+++ App.tsx
@@ -4,10 +4,25 @@ import './gesture-handler';
 import RootProvider from './src/context';
 import AppNavigator from './src/navigation';
 import SplashScreen from 'react-native-splash-screen';
+import {checkFirstLaunch} from '@/utils/storage';
+import {requestNotificationPermission} from './src/utils/notification';
+
 function App(): React.JSX.Element {
   useEffect(() => {
-    SplashScreen.hide();
-    console.log('Splash screen hide');
+    const initializeApp = async () => {
+      try {
+        //TODO: fcm 토큰 등록 위치 논의 후 따로 분리 예정
+        const isFirstLaunch = await checkFirstLaunch();
+        if (isFirstLaunch) {
+          await requestNotificationPermission();
+        }
+      } catch (error) {
+        console.error('splash screen error:', error);
+      } finally {
+        SplashScreen.hide();
+      }
+    };
+    initializeApp();
   }, []);
   return (
     <RootProvider>

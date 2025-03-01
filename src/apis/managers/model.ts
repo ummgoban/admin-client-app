08b283528b69d0d8caa3b72a574e654ce08aa1a7
l-lyun <kimdohyun032@gmail.com>
2025-03-01 18:05:22 +0900

    feat: 직원 관리 client.ts 작성

--- src/apis/managers/model.ts
+++ src/apis/managers/model.ts
@@ -1,35 +1,15 @@
 import {ManagerInfo} from '@/types/Managers';
 
-export type WithDrawManagerRequest = {
-  marketId: number;
-};
-
 export type DeleteManagerRequest = {
   marketId: number;
   memberId: number;
 };
 
-export type ReadMangersRequest = {
-  marketId: number;
-};
-
 export type ReadManagersResponse = ManagerInfo[];
-
-export type ReadCreatePendingMangerRequest = {
-  marketId: number;
-};
 
 export type ReadCreatePendingMangerResponse = {
   memberName: string;
   ttl: number;
-};
-
-export type CreateMangerResponse = {
-  marketId: number;
-};
-
-export type GenerateAuthCodeRequest = {
-  marketId: number;
 };
 
 export type GenerateAuthCodeResponse = {
@@ -37,7 +17,7 @@ export type GenerateAuthCodeResponse = {
   createdAt: string;
 };
 
-export type ValidationAuthCodeRequset = {
+export type ValidationAuthCodeRequest = {
   marketName: string;
   authCode: string;
 };

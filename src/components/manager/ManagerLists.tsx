426398cd6484a6f787b376cac632ec92f5056159
l-lyun <kimdohyun032@gmail.com>
2025-03-04 20:48:20 +0900

    feat: 모달 추가

--- src/components/manager/ManagerLists.tsx
+++ src/components/manager/ManagerLists.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import S from './ManagerLists.style';
 import ManagerList from './ManagerList';
 import {ManagerInfo} from '@/types/Managers';
+import {BottomButton} from '../common';
+import ManagerModal from './ManagerModal';
 
 type ManagerListsProps = {
   managers: ManagerInfo[] | null | undefined;
@@ -9,24 +11,41 @@ type ManagerListsProps = {
 };
 
 const ManagerLists = ({managers, marketId}: ManagerListsProps) => {
-  return (
-    <S.ListsContainer>
-      <S.HeaderRow>
-        <S.HeaderText>이름</S.HeaderText>
-        <S.HeaderText>직책</S.HeaderText>
-        <S.HeaderText>삭제하기</S.HeaderText>
-      </S.HeaderRow>
+  const [visible, setVisible] = useState(false);
 
-      {managers?.map(manager => (
-        <ManagerList
-          key={manager.id}
-          memberId={manager.id}
-          marketId={marketId}
-          name={manager.name}
-          marketRole={manager.marketRole}
-        />
-      ))}
-    </S.ListsContainer>
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+
+  if (!marketId) {
+    marketId = 0;
+  }
+
+  return (
+    <>
+      <S.ListsContainer>
+        <S.HeaderRow>
+          <S.HeaderText>이름</S.HeaderText>
+          <S.HeaderText>직책</S.HeaderText>
+          <S.HeaderText>삭제하기</S.HeaderText>
+        </S.HeaderRow>
+
+        {managers?.map(manager => (
+          <ManagerList
+            key={manager.id}
+            memberId={manager.id}
+            marketId={marketId}
+            name={manager.name}
+            marketRole={manager.marketRole}
+          />
+        ))}
+        <BottomButton onPress={showModal}>직원 추가하기</BottomButton>
+      </S.ListsContainer>
+      <ManagerModal
+        visible={visible}
+        onDismiss={hideModal}
+        marketId={marketId}
+      />
+    </>
   );
 };
 

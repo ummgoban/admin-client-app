58d4872207ea778923f1b3c5cfb7681c3ab57267
l-lyun <kimdohyun032@gmail.com>
2025-03-07 10:14:45 +0900

    commnet: 주석 수정

--- src/apis/managers/query.ts
+++ src/apis/managers/query.ts
@@ -33,7 +33,10 @@ export const useDeleteManager = ({
   });
 };
 
-// FIXME: 페이지 단에서 queryKey 사용시 profile.id null handling
+/**
+ * FIXME: Query data cannot be undefined. Please make sure to return a value other than undefined from your query function. Affected query key: ["readManagers",null]
+ * queryKey에 null이 들어가면 안됩니다.
+ * */
 export const useReadManagers = (marketId: number) => {
   return useQuery({
     queryKey: ['readManagers', marketId],

dd5c734319cfda607e168058894421de6077ce25
l-lyun <kimdohyun032@gmail.com>
2025-08-04 16:46:46 +0900

    feat: 휴업 체크박스 생성 및 영업 시간 테이블화

--- src/screens/MarketInfoScreen/MarketInfoScreen.style.tsx
+++ src/screens/MarketInfoScreen/MarketInfoScreen.style.tsx
@@ -8,6 +8,7 @@ const S = {
   Container: styled.View`
     flex: 1;
   `,
+
   ScrollView: styled.ScrollView`
     padding: 16px;
 
@@ -21,19 +22,75 @@ const S = {
     gap: 8px;
   `,
 
+  BusinessTimeInput: styled.View`
+    padding: 12px 40px;
+  `,
+
+  TimeHeader: styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 8px;
+    padding: 0 16px;
+  `,
+
+  DayHeaderText: styled(Text)`
+    flex: 1;
+    text-align: left;
+    font-weight: bold;
+    font-size: 14px;
+  `,
+
+  TimeHeaderText: styled(Text)`
+    flex: 1;
+    text-align: center;
+    font-weight: bold;
+    font-size: 14px;
+  `,
+
+  ClosedHeaderText: styled(Text)`
+    flex: 1;
+    text-align: right;
+    font-weight: bold;
+    font-size: 14px;
+  `,
+
   TimeContainer: styled.View`
-    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 4px;
+    padding: 0 8px;
+  `,
+
+  DayText: styled(Text)`
+    flex: 1;
+    text-align: left;
+  `,
+
+  TimeRange: styled.View`
+    flex: 1;
     flex-direction: row;
     align-items: center;
     justify-content: center;
   `,
 
+  ClosedCheckboxWrapper: styled.View`
+    display: flex;
+    flex: 1;
+    align-items: flex-end;
+  `,
+
+  ClosedCheckboxContainer: styled.View`
+    width: 36px;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    border-radius: 8px;
+    margin: 4px;
+  `,
+
   TimePickerButton: styled(Button)``,
 
-  BusinessTimeInput: styled.View``,
-
   ImageCardGrid: styled.View`
-    display: flex;
     flex-direction: row;
     flex-wrap: wrap;
 
@@ -42,17 +99,12 @@ const S = {
     margin: 16px 0;
   `,
 
-  DayText: styled(Text)`
-    ${({theme}) => theme.fonts.body2}
-  `,
-
   ImageCardPlusButton: styled(Button)`
     border: 1px solid #e0e0e0;
-
     width: 100%;
-
     margin: 16px 0;
   `,
+
   ImageCard: styled.View<{width: number}>`
     position: relative;
 
@@ -65,6 +117,7 @@ const S = {
 
     box-sizing: border-box;
 
+    height: 92px;
     border: 1px solid #e0e0e0;
   `,
 
@@ -83,9 +136,8 @@ const S = {
 
     border-radius: 16px;
 
-    background-color: #fff;
-
     display: flex;
+    background-color: #fff;
     justify-content: center;
     align-items: center;
   `,

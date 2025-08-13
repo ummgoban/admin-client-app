c6e0471403aab7483964d835af5cebdda007a818
l-lyun <kimdohyun032@gmail.com>
2025-08-13 12:48:03 +0900

    fix: menu로 네비게이터 이름 변경

--- src/types/ProductType.ts
+++ src/types/ProductType.ts
@@ -1,7 +1,7 @@
 import {ProductType} from '@ummgoban/shared';
 
 export type MenuType = ProductType & {
-  reservationStatus: 'PENDING' | 'ACTIVE';
+  reservationStatus: 'PENDING' | 'ACTIVE' | null;
 };
 
 export type TagType = {
